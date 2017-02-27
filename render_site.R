253f8ce8249ce5a544f13ebc292a0dc42d27f2e0
account for CONDOR jobs <dmaziuk@bmrb.wisc.edu>
2017-02-27 12:13:28 -0600

    Update render script to specify pandoc library location. Add setup instructions document.

--- render_site.R
+++ render_site.R
@@ -1,3 +1,7 @@
-setwd('/home/kumaran/git/BMRB-Histogram')
+# Set the current directory as the working directory
+setwd('./')
+# Specify the location of the pandoc libraries
+Sys.setenv(RSTUDIO_PANDOC="/usr/lib/rstudio/bin/pandoc")
+# Generate the pages
 rmarkdown::render_site()
 
